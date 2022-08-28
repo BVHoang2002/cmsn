cc6adf2565d07bda69512d4587575e428197e24a
code <hoangk0202@gmail.com>
2022-08-28 13:28:40 +0700

    up

---
+++ main.js
@@ -0,0 +1,32 @@
+const menuE= document.querySelectorAll('.menu-item');
+const containerE= document.querySelector('.container');
+const navE=document.querySelector('.nav')
+menuE[0].onclick = function(){
+    containerE.classList.add('show');
+    navE.classList.add('show');
+    this.parentElement.classList.add('show');
+}
+menuE[1].onclick = function(){
+    containerE.classList.remove('show');
+    navE.classList.remove('show');
+    this.parentElement.classList.remove('show');
+}
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "flex";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
